e46e46f8e35841f18fb31bd87029be0eed7bb0d5
SZF2 A <szf2_a@oktatas.local>
2022-03-17 17:31:59 +0100

    1. név deklarálása

---
+++ src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javakonfliktuskezeles;
+
+/**
+ *
+ * @author SZf2_a
+ */
+public class JavaKonfliktusKezeles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
