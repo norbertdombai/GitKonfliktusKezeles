9593bf5cfc3afd08fcde1c26d3c38793ad96cf5a
SZF2 A <szf2_a@oktatas.local>
2022-03-17 17:43:25 +0100

    git konfliktus kezeles:nevek inicializálva

--- src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
+++ src/javakonfliktuskezeles/JavaKonfliktusKezeles.java
@@ -5,13 +5,10 @@ public class JavaKonfliktusKezeles {
 
     
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         String nev1 = "D.N.";  
-        String nev2;
-=======
-        String nev1;  
         String nev2 = "X.Y";
->>>>>>> e65a2c7c0052e18f405a94bdf2a29f4f3b05b1b1
+
     }
     
 }
